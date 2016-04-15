5e4cd0e1feb6f47e2b4b2c9d8b8bd03f2452205d
Marcelo <marce.mmad@gmail.com>
2016-04-15 14:26:33 -0400

    borrado sitio anterior en wordpress

--- wp-content/plugins/accesspress-custom-css/inc/backend/activation.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-$apccss_settings = array();
-$apccss_settings['css'] = '';
-update_option( APCCSS_SETTINGS, $apccss_settings );
-?>
